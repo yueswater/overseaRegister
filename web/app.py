dfb186370cbc443a1c292c236693e32ce049cc1e
xiaolong70701 <xiaolong70701@gmail.com>
2025-07-29 01:19:10 +0800

    fix: 修改 port

--- web/app.py
+++ web/app.py
@@ -20,5 +20,9 @@ app.register_blueprint(registered_list_bp)
 def ping():
     return "pong", 200
 
+
 if __name__ == "__main__":
-    app.run(port=10301)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
